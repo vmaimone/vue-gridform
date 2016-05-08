70b299978c51592b07f04a4982451ffdc5a5f47d
Vin Maimone <vmaimone@rochester.rr.com>
2016-05-08 10:32:27 -0400

    output to global VueGridform variable

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
+    library: 'VueGridform',
     libraryTarget: "umd",
     filename: '[name].js'
   },
